e2d6ab4c86d138db36bc83f5e4bd9b2cb02b2ca1
Kjetil Lorentzen <kjeloren@gmail.com>
2020-05-24 16:52:32 +0200

    DRF permission settings

--- olmonopolet_api/settings.py
+++ olmonopolet_api/settings.py
@@ -122,3 +122,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Django REST Framework Settings
+REST_FRAMEWORK={
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ]
+}
