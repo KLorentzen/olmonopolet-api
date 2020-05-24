13266ad0198a03a5367e1013d18f325893cdecd5
Kjetil Lorentzen <kjeloren@gmail.com>
2020-05-24 16:32:42 +0200

    added django rest framework to installed apps

--- olmonopolet_api/settings.py
+++ olmonopolet_api/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # 3rd Party Apps
+    'rest_framework',
+
 ]
 
 MIDDLEWARE = [
