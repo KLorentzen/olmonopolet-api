d61f3497e24eece2240e4c2022e20e18cfaced24
Kjetil Lorentzen <kjeloren@gmail.com>
2020-06-06 18:43:09 +0200

    added auth to DRF  browsable API

--- olmonopolet_api/urls.py
+++ olmonopolet_api/urls.py
@@ -19,4 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/',include('api.urls')),
+    path('api-auth',include('rest_framework.urls')),
+    #TODO: Add 404 for fallthrough paths
 ]
