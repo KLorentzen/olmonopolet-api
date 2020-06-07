fdf894e721bc1a66a3f2d0e63824d8ec7d7250b7
Kjetil Lorentzen <kjeloren@gmail.com>
2020-06-07 20:08:41 +0200

    product model added to beers and admin

--- beers/admin.py
+++ beers/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
-from .models import Beer
+from .models import Beer,Product
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('product_id','main_category','added_date')
+
+
+class BeerAdmin(admin.ModelAdmin):
+    list_display = ('name','brewery','style','alc_volume','selection','added_date')
+    
+
 
 # Register your models here.
-admin.site.register(Beer)
+admin.site.register(Beer,BeerAdmin)
+admin.site.register(Product,ProductAdmin)
+
--- beers/models.py
+++ beers/models.py
@@ -3,15 +3,32 @@ from django.db import models
 # Create your models here.
 
 class Beer(models.Model):
-    beer_ID = models.IntegerField(primary_key=True)
+    # TODO: Add Product as foreign key?
+    beer_id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=250)
-    brewery = models.CharField(max_length=250)
-    style = models.CharField(max_length=250)
-    alc_volume = models.FloatField()
-    volume = models.FloatField()
-    url = models.URLField(max_length=250)
+    brewery = models.CharField(max_length=250, blank=True)
+    style = models.CharField(help_text="Beer style",max_length=250, blank=True)
+    alc_volume = models.FloatField(help_text="Alcohol volume in %", null=True,blank=True)
+    volume = models.FloatField(help_text="Volume in liters",null=True ,blank=True)
+    selection = models.CharField(max_length=250, blank=True)
+    url = models.URLField(max_length=250, blank=True)
+
     
     added_date = models.DateField(auto_now=False, auto_now_add=True)
     
     def __str__(self):
         return self.name
+
+class Product(models.Model):
+    '''
+    Utility model holding all product id's fetched from Vinmonopolet.
+    Products include categories other than "øl".
+    '''
+    product_id = models.IntegerField()
+    main_category = models.CharField(max_length=250)
+
+    added_date = models.DateField(auto_now=False, auto_now_add=True)
+    
+    def __str__(self):
+        return str(self.product_id)
+    
