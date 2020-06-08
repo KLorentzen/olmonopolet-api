92806bc42d700779706da5d1c7fcd8535cc135df
Kjetil Lorentzen <kjeloren@gmail.com>
2020-06-08 06:40:33 +0200

    updated Store model address fields

--- stores/models.py
+++ stores/models.py
@@ -5,9 +5,10 @@ class Store(models.Model):
     '''Vinmonopolet stores'''
     store_id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=250)
-    category = models.IntegerField(help_text='Store category, higher number is better quality')
+    category = models.IntegerField(help_text='Store category, higher number is better quality',blank=True)
     city = models.CharField( max_length=250)
-    address = models.CharField( max_length=250)
+    street_address = models.CharField( max_length=250)
+    postalcode = models.IntegerField()
 
     def __str__(self):
         return self.name
--- stores/serializers.py
+++ stores/serializers.py
@@ -3,5 +3,5 @@ from .models import Store
 
 class StoreSerializer(serializers.ModelSerializer):
     class Meta:
-        fields=('store_id','name','category','city','address')
+        fields=('store_id','name','category','city','street_address', 'postalcode')
         model = Store
