f1e15a1e3a73b189c4a34fccdfbb2cbdca40680e
Kjetil Lorentzen <kjeloren@gmail.com>
2020-06-22 17:44:30 +0200

    beer model help text

--- beers/models.py
+++ beers/models.py
@@ -20,12 +20,12 @@ class Beer(models.Model):
     name = models.CharField(max_length=250)
     brewery = models.CharField(max_length=250, blank=True)
     country = models.CharField(max_length=250, blank=True)
-    style = models.CharField(help_text="Beer style",max_length=250, blank=True)
+    style = models.CharField(help_text="Beer style listed at Vinmonopolet",max_length=250, blank=True)
     alc_volume = models.FloatField(help_text="Alcohol volume in %", null=True,blank=True)
     price = models.FloatField(help_text="Product prize in NOK", default= 0.00,  blank=True)
     volume = models.FloatField(help_text="Volume in liters",null=True ,blank=True)
     selection = models.CharField(help_text='Product selection and ordering range',max_length=250, blank=True)
-    url = models.URLField(max_length=250, blank=True)
+    url = models.URLField(help_text='URL extension to https://vinmonopolet.no',max_length=250, blank=True)
 
     
     added_date = models.DateField(auto_now=False, auto_now_add=True)
