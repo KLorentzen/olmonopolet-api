f3d0468189fc336c9ad062c6152dc2a600a09859
Kjetil Lorentzen <kjeloren@gmail.com>
2020-06-23 10:32:12 +0200

    url conf for sales app stock module to support sales calc

--- api/urls.py
+++ api/urls.py
@@ -4,5 +4,6 @@ urlpatterns = [
     path('beers/',include('beers.urls')),
     path("stores/", include('stores.urls')),
     path("beer-stock/", include('stock.urls')),
-    path('untappd/', include('untappd.urls'))
+    path('untappd/', include('untappd.urls')),
+    path("sales/", include('sales.urls')),
 ]
