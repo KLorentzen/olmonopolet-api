02977d5971e9f70af7b983ee3d2ae94ac46e1096
Kjetil Lorentzen <kjeloren@gmail.com>
2020-06-24 07:41:37 +0200

    admin path update

--- olmonopolet_api/urls.py
+++ olmonopolet_api/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('kjelleren/', admin.site.urls),
     path('api/v1/',include('api.urls')),
     path('api-auth',include('rest_framework.urls')),
     #TODO: Add 404 for fallthrough paths
