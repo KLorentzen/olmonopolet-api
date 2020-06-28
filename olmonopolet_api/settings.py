d0f4f38c3a68eade717b2b7b1f3dafcbf462ae66
Kjetil Lorentzen <kjeloren@gmail.com>
2020-06-28 09:44:27 +0200

    Sendgrid configuration - SMTP Relay

--- olmonopolet_api/settings.py
+++ olmonopolet_api/settings.py
@@ -151,3 +151,12 @@ REST_FRAMEWORK={
         'django_filters.rest_framework.DjangoFilterBackend'
     ],
 }
+
+
+# Email configuration with Sendgrid
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST =  os.environ.get('SG_EMAIL_HOST')
+EMAIL_HOST_USER = os.environ.get('SG_EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('SENDGRID_API_KEY')
+EMAIL_PORT = os.environ.get('SG_EMAIL_PORT')
+EMAIL_USE_TLS = True if os.environ.get('SG_EMAIL_USE_TLS').lower() == 'true' else False
