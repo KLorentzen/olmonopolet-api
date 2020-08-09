84adc8a0d6c7b67236b5e1b0e5b3c08c1bf0d103
Kjetil Lorentzen <kjeloren@gmail.com>
2020-08-09 17:59:20 +0200

    mgmt cmd to fetch untappd data - update_untappd - added logging

--- olmonopolet/untappd_scraper/details.py
+++ olmonopolet/untappd_scraper/details.py
@@ -19,27 +19,30 @@ def get_beer_details(url):
     untappd_html = BeautifulSoup(raw_untappd_response,'html.parser')
 
     # Retrieve Data
+    try:
+        untappd_details["brewery"] = untappd_html.find("div",class_="content").find('div', class_='name').find('a').string
+        untappd_details["brewery_url"] = untappd_html.find("div",class_="content").find('div', class_='name').find('a')['href']
+        untappd_details["style"] = untappd_html.find("div",class_="content").find('div', class_='name').find('p', class_='style').string
+        # TODO: Få inn description korrekt. Per nå er det problematisk å få kun tekst siden det også er en <a> tag i div.
+        untappd_details["description"] = untappd_html.find("div", class_="content").find("div",class_="bottom").find("div",class_="desc").find("div",class_="beer-descrption-read-less").contents
 
-    untappd_details["brewery"] = untappd_html.find("div",class_="content").find('div', class_='name').find('a').string
-    untappd_details["brewery_url"] = untappd_html.find("div",class_="content").find('div', class_='name').find('a')['href']
-    untappd_details["style"] = untappd_html.find("div",class_="content").find('div', class_='name').find('p', class_='style').string
-    untappd_details["description"] = untappd_html.find("div", class_="content").find("div",class_="bottom").find("div",class_="desc").find("div",class_="beer-descrption-read-less").string
+        # TODO: Handle img_url which is placeholder for missing img at Untappd
+        untappd_details["img_url"] = untappd_html.find("div",class_="content").find('a',class_ = 'label').find('img')['src'] 
 
-    # TODO: Handle img_url which is placeholder for missing img at Untappd
-    untappd_details["img_url"] = untappd_html.find("div",class_="content").find('a',class_ = 'label').find('img')['src'] 
+        untappd_details["rating"] = untappd_html.find("div", class_="content").find("div",class_="details").find("div",class_="caps")['data-rating']
+        # Text is typically 'xxx Ratings', hence I need to split the string and get the first element
+        untappd_details["num_regs"] = untappd_html.find("div", class_="content").find("div",class_="details").find("p",class_="raters").string.split()[0]
+        untappd_details["check_in_total"] = untappd_html.find("div",class_="content").find("div",class_="stats").find("span",class_="count").string
+        untappd_details["check_in_unique"] = untappd_html.find("div",class_="content").find("div",class_="stats").find_all("span",class_="count")[1].string
 
-    untappd_details["rating"] = untappd_html.find("div", class_="content").find("div",class_="details").find("div",class_="caps")['data-rating']
-    # Text is typically 'xxx Ratings', hence I need to split the string and get the first element
-    untappd_details["num_regs"] = untappd_html.find("div", class_="content").find("div",class_="details").find("p",class_="raters").string.split()[0]
-    untappd_details["check_in_total"] = untappd_html.find("div",class_="content").find("div",class_="stats").find("span",class_="count").string
-    untappd_details["check_in_unique"] = untappd_html.find("div",class_="content").find("div",class_="stats").find_all("span",class_="count")[1].string
+        # TODO: Add validations and try/excepts
 
-    # TODO: Add validations and try/excepts
-
-    # Stripping thousand separator ',' from numbers
-    untappd_details["num_regs"] = untappd_details["num_regs"].replace(',','')
-    untappd_details["check_in_total"] = untappd_details["check_in_total"].replace(',','')
-    untappd_details["check_in_unique"] = untappd_details["check_in_unique"].replace(',','')
+        # Stripping thousand separator ',' from numbers
+        untappd_details["num_regs"] = untappd_details["num_regs"].replace(',','')
+        untappd_details["check_in_total"] = untappd_details["check_in_total"].replace(',','')
+        untappd_details["check_in_unique"] = untappd_details["check_in_unique"].replace(',','')
+    except Exception as err:
+        print(f"Exception cought while updating untappd data: {err}")
 
 
     return untappd_details
--- untappd/management/commands/update_untappd.py
+++ untappd/management/commands/update_untappd.py
@@ -2,33 +2,48 @@ from django.core.management.base import BaseCommand
 from django.db.utils import IntegrityError
 from olmonopolet.untappd_scraper import details
 from untappd.models import Untappd, UntappdMapping
+from datetime import datetime
+import time
 
 class Command(BaseCommand):
     help = 'Update Untappd Beer Details'
 
     def handle(self, *args, **options):
         # Get verified Beer Mappings
-        mappings = UntappdMapping.objects.filter(verified=True)
+        mappings = UntappdMapping.objects.filter(auto_match=True) | UntappdMapping.objects.filter(verified=True)
         
+        # Log when the job is executed
+        start_time = datetime.now()
+        self.stdout.write(f"Updating Untappd data @ {datetime.now()}")
+
         for mapping in mappings:
             
             untappd = details.get_beer_details(mapping.url)
 
-            obj = Untappd.objects.update_or_create(
-                beer_id = mapping.beer_id,
-                url = mapping.url,
-                defaults={
-                    'brewery':untappd["brewery"],
-                    'brewery_url': 'https://www.untappd.com' + untappd["brewery_url"],
-                    'style':untappd["style"],
-                    'description': untappd["description"] if untappd["description"] else '',
-                    'img_url':untappd["img_url"],
-                    'rating': untappd["rating"],
-                    'num_ratings': untappd["num_regs"],
-                    'check_in_total': untappd["check_in_total"],
-                    'check_in_unique': untappd["check_in_unique"]
-                }
-            )
-            self.stdout.write(f"Oppdaterer Untappd data for {mapping.beer_id.name}")
-
+            try:
+                obj = Untappd.objects.update_or_create(
+                    beer_id = mapping.beer_id,
+                    url = mapping.url,
+                    defaults={
+                        'brewery': untappd["brewery"],
+                        'brewery_url': 'https://www.untappd.com' + untappd["brewery_url"],
+                        'style':untappd["style"],
+                        'description': untappd["description"] if untappd["description"] else '',
+                        'img_url':untappd["img_url"],
+                        'rating': untappd["rating"],
+                        'num_ratings': untappd["num_regs"],
+                        'check_in_total': untappd["check_in_total"],
+                        'check_in_unique': untappd["check_in_unique"]
+                    }
+                )
+                self.stdout.write(f"Oppdaterer Untappd data for {mapping.beer_id.name}")
+            except Exception as err:
+                self.stdout.write(f"Could not create or update Untappd data for '{mapping.beer_id.name}'. Got the following exception: {err} ")
+            
+            # Reduce load on Untappd and DB
+            time.sleep(0.5)
+        
+        end_time = datetime.now()
+        self.stdout.write(f"Untappd update took {end_time - start_time} seconds")
+        
         return
