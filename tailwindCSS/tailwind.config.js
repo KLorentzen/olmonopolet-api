c81df36b516741f4c33682a194dcc91e29fa9d07
Kjetil Lorentzen <kjeloren@gmail.com>
2021-01-23 18:15:14 +0100

    added TailwindCSS

---
+++ tailwindCSS/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: [
+    '../templates/**/*.html',
++   '../templates/**/*.js',
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
