3a98eaaef0887f8dcd72dc3f890524cf700c470f
Kjetil Lorentzen <kjeloren@gmail.com>
2021-02-16 19:18:14 +0100

    search bar in landing page - possible to search for vinmonopolet store

--- stores/urls.py
+++ stores/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
-from .views import StoreView
+from .views import StoreView, store_search
 
 urlpatterns = [
-    path('', StoreView.as_view(), name='home')
+    path('', StoreView.as_view(), name='home'),
+    path('search/', store_search, name='store_search'),
 ]
--- stores/views.py
+++ stores/views.py
@@ -5,6 +5,20 @@ from rest_framework import generics
 from django.views.generic import TemplateView
 
 
+# View Functions
+def store_search(request):
+    '''
+    Used by search bar in landing page (home)
+    '''
+    # Returns no stores if query string is empty ('')
+    stores = None
+
+    # Check that query is not empty
+    if request.POST["query"] != '':
+        # Fetch all stores that math the query string
+        stores = Store.objects.filter(name__icontains=request.POST["query"])
+    return render(request, 'stubs/store_search.html', {'stores': stores})
+
 # Regular Views
 class StoreView(TemplateView):
     template_name='home.html'
