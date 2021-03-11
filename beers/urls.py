c4a1685b22e4b86ac45edaddc54e4b92a7c9e67f
Kjetil Lorentzen <kjeloren@gmail.com>
2021-03-11 19:26:44 +0100

    'ølslepp' initial version

--- beers/urls.py
+++ beers/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
-from .views import BeerStockView, store_beers, beer_stock_search
+from .views import BeerStockView, ReleaseListView, store_beers, beer_stock_search
 
 urlpatterns = [
+    path('<int:store_id>/release',ReleaseListView.as_view(), name='release'),
     path('<int:store_id>/store_beers',store_beers, name='store_beers'),
     path('<int:store_id>/beer_stock_search',beer_stock_search, name='beer_stock_search'),
     path('<int:store_id>/',BeerStockView.as_view(), name='beer_stock'),
