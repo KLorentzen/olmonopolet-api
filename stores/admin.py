aee1c23b1fe98b9c7a4994703a6618bd29699861
Kjetil Lorentzen <kjeloren@gmail.com>
2021-04-16 07:26:59 +0200

    admin search for Stores

--- stores/admin.py
+++ stores/admin.py
@@ -3,6 +3,7 @@ from .models import Store
 
 class StoreAdmin(admin.ModelAdmin):
     list_display = ('name', 'city', 'category', 'active')
+    search_fields = ('name',)
 
 
 # Register your models here.
